164aa37fe2d69b65166124a5aa5136b1189c9e61
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2025-03-01 09:30:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-var a = process.env.API_KEY;
-console.log(a)
+const apiKey = process.env.API_KEY;
+
+if (!apiKey) {
+  console.error("API key not found!");
+  process.exit(1);
+}
+
+console.log(`Using API key: ${apiKey}`);
+// Ваш код для работы с API
